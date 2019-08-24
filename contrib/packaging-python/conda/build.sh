7aac0da4223c744f3bd7b73b2cdcbccccaf28561
Simone Benatti <simone.benatti@studenti.unipr.it>
2019-08-24 11:48:05 +0200

    Build and install in base env

--- contrib/packaging-python/conda/build.sh
+++ contrib/packaging-python/conda/build.sh
@@ -36,11 +36,11 @@ cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
  -DBUILD_BENCHMARKING=OFF \
  -DBUILD_GMOCK=OFF \
  -DENABLE_MODULE_CASCADE=ON \
- -DCASCADE_INCLUDE_DIR=/home/appveyor/miniconda3/envs/myenv/include/oce \
- -DCASCADE_LIBDIR=/home/appveyor/miniconda3/envs/myenv/lib \
+ -DCASCADE_INCLUDE_DIR=/home/appveyor/miniconda3/include/oce \
+ -DCASCADE_LIBDIR=/home/appveyor/miniconda3/lib \
  -DENABLE_MODULE_MKL=ON \
- -DMKL_INCLUDE_DIR=/home/appveyor/miniconda3/envs/myenv/include \
- -DMKL_RT_LIBRARY=/home/appveyor/miniconda3/envs/myenv/lib/libmkl_rt.so \
+ -DMKL_INCLUDE_DIR=/home/appveyor/miniconda3/include \
+ -DMKL_RT_LIBRARY=/home/appveyor/miniconda3/lib/libmkl_rt.so \
  ./..
 # Build step
 # on linux travis, limit the number of concurrent jobs otherwise
